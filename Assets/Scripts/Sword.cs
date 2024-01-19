0ac25a6233638b9e81cb4b2a54d75abeba30f3be
Redream <91314959+v-golyadkin@users.noreply.github.com>
2024-01-19 20:32:24 +0300

    Update

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -6,10 +6,28 @@ using UnityEngine.EventSystems;
 
 public class Sword : MonoBehaviour
 {
+    [SerializeField] private Transform weaponCollider;
+
     public event EventHandler OnSwordSwing;
 
     public void Attack()
     {
         OnSwordSwing?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void Update()
+    {
+        AdjustPlayerFacingDirection();
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePosition = GameInput.Instance.GetMousePosition();
+        Vector3 playerPosition = Player.Instance.GetPlayerPosition();
+
+        if (mousePosition.x < playerPosition.x)
+            weaponCollider.transform.rotation = Quaternion.Euler(0, -180, 0);
+        else
+            weaponCollider.transform.rotation = Quaternion.Euler(0, 0, 0);
     }
 }
