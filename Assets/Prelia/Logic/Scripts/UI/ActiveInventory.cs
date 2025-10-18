1b459cfdeb7ba8a47909ec5b19e025a02e18525f
Redream <mr.golyadkina@mail.ru>
2025-10-18 16:16:34 +0300

    fix: bug with active inventory

--- Assets/Prelia/Logic/Scripts/UI/ActiveInventory.cs
+++ Assets/Prelia/Logic/Scripts/UI/ActiveInventory.cs
@@ -3,13 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActiveInventory : Singleton<ActiveInventory>
+public class ActiveInventory : PersistentSingleton<ActiveInventory>
 {
     private int _activeSlotIndexNumber = 0;
 
     private PlayerActions _playerActions;
 
-    private void Awake()
+    protected override void Awake()
     {
         base.Awake();
 
@@ -18,14 +18,30 @@ public class ActiveInventory : Singleton<ActiveInventory>
 
     private void Start()
     {
-        _playerActions.Inventory.Keyboard.performed += context => ToggleActiveSlot((int)context.ReadValue<float>());
-
-        
+        _playerActions.Inventory.Keyboard.performed += OnKeyboardPerformed;
     }
 
     private void OnEnable()
     {
-        _playerActions.Enable();       
+        _playerActions?.Enable();       
+    }
+
+    private void OnDisable()
+    {
+        _playerActions?.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        if(_playerActions != null)
+        {
+            _playerActions.Inventory.Keyboard.performed -= OnKeyboardPerformed;
+        }
+    }
+
+    private void OnKeyboardPerformed(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        ToggleActiveSlot((int)context.ReadValue<float>());
     }
 
     private void ToggleActiveSlot(int numberValue)
@@ -37,13 +53,32 @@ public class ActiveInventory : Singleton<ActiveInventory>
     {
         _activeSlotIndexNumber = indexNumber;
 
-        foreach(Transform inventorySlot in this.transform)
+        if(this.transform.childCount == 0)
         {
-            inventorySlot.GetChild(0).gameObject.SetActive(false);
+            Debug.LogWarning("No inventory slots found!");
+            return;
         }
 
-        this.transform.GetChild(indexNumber).GetChild(0).gameObject.SetActive(true);
+        if(indexNumber < 0 || indexNumber >= this.transform.childCount)
+        {
+            Debug.Log($"Invalid slot index: {indexNumber}");
+            return;
+        }
 
+        foreach(Transform inventorySlot in this.transform)
+        {
+            if(inventorySlot.childCount > 0)
+            {
+                inventorySlot.GetChild(0).gameObject.SetActive(false);
+            }
+        }
+
+        Transform selectedSlot = this.transform.GetChild(indexNumber);
+        if(selectedSlot.childCount > 0)
+        {
+            this.transform.GetChild(indexNumber).GetChild(0).gameObject.SetActive(true);
+        }
+        
         ChangeActiveWeapon();
     }
 
